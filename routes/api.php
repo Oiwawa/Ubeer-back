d46664911759d23ca51b54de6fd1eb6925737343
Oiwawa <monstruousbird@protonmail.com>
2022-03-15 13:46:04 +0100

    route / updte

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BreweryController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('user', UserController::class);
+
+Route::apiResource('brewery', BreweryController::class);
